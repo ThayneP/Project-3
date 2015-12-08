46d8bf29964fb9e5c6bdef24875d4b344b4990e0
Thayne Price <thayne@vt.edu>
2015-12-08 16:30:01 -0500

    The initial commit

---
+++ hangman.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+
+# Thayne Price - 12/8/2015 - Intro to Unix
+# This project makes use of GitHub as its version control.
+# In this project, I will implement the game hangman. Initially the user will be allowed to select 
+# the difficulty by typing in easy/medium/hard. Then the program will print out a empty gallows with # blanks displayed for the unknow letters of the word. For each mistake the gallows will be displayed# with an additional body part. The user will be given 5 mistakes before the hangman image is
+# completed and "Game Over: You word was ___________". The words to be guessed are selected at random
+# from an input file.
+
+
